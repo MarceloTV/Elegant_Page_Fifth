d5cf99d1b1c45bd7c27f57556a088c62afd6331e
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2022-03-15 12:04:07 -0400

    responsive

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ const carousel = new Glide(".glide", {
 });
 
 carousel.mount();
+
+const open_menu = document.getElementById("open");
+const mobile_menu = document.getElementById("menu");
+const close_menu = document.getElementById("close");
+
+function menu(e){
+    mobile_menu.classList.toggle("show");
+}
+
+open_menu.onclick = menu;
+close_menu.onclick = menu;
